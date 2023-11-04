22f1528d2874730a139e9c875a6db1368bdc1c71
iineineno03k <iineineno03k@gmail.com>
2023-11-04 16:38:13 +0900

    カード改善

--- src/component/SpecialMoveCard.tsx
+++ src/component/SpecialMoveCard.tsx
@@ -31,33 +31,55 @@ const SpecialMoveCard: React.FC<Props> = ({ name, furigana, heading, description
     };
 
     return (
-        <Box flexDirection="column" alignItems="start">
-            <Card sx={{ maxWidth: '100vw', mb: 2 }}>
+        <Box flexDirection="column" alignItems="start" sx={{ width: '100%' }}>
+            <Card
+                sx={{
+                    mb: 2,
+                    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+                    borderRadius: '16px',
+                    overflow: 'hidden',
+                    border: '1px solid rgba(0, 0, 0, 0.12)',
+                    transition: '0.3s',
+                    '&:hover': {
+                        boxShadow: '0 8px 16px 0 rgba(0, 0, 0, 0.2)',
+                        transform: 'translateY(-2px)',
+                    },
+                }}
+            >
                 <Box display="flex" flexDirection="row" alignItems="center">
                     <CardMedia
                         component="img"
                         sx={{
                             width: 150,
                             height: 150,
-                            objectFit: 'cover'
+                            objectFit: 'cover',
+                            borderRadius: '8px',
+                            mr: 1.5
                         }}
                         image={croppedImg}
                         alt={name}
                     />
 
-                    <CardContent>
-                        <Typography variant="caption" display="block">
+                    <CardContent sx={{ flexGrow: 1, p: 1, minWidth: 0 }}>
+                        <Typography gutterBottom variant="caption" display="block" align="center">
                             {furigana}
                         </Typography>
-                        <Typography variant="h5" component="div">
+                        <Typography gutterBottom variant="h5" component="div">
                             {name}
                         </Typography>
-                        <Button variant="outlined" sx={{ mt: 2 }} onClick={handleOpen}>
-                            詳細
-                        </Button>
+                        <Box mt={1.5} display="flex" flexDirection="column">
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={handleOpen}
+                                sx={{ mb: 1, borderRadius: '20px' }}
+                            >
+                                詳細
+                            </Button>
+                        </Box>
                     </CardContent>
                 </Box>
-                <Typography textAlign={"center"} variant="h6" component="div" sx={{ ml: 2 }}>
+                <Typography textAlign={"center"} variant="h6" component="div" sx={{ ml: 2, mr: 2 }}>
                     {heading}
                 </Typography>
             </Card>
@@ -65,32 +87,65 @@ const SpecialMoveCard: React.FC<Props> = ({ name, furigana, heading, description
                 open={open}
                 onClose={handleClose}
                 fullScreen
+                sx={{
+                    '& .MuiDialog-container': {
+                        display: 'flex',
+                        justifyContent: 'center',
+                    },
+                    '& .MuiPaper-root': {
+                        backgroundColor: 'rgba(255, 255, 255, 0.95)',
+                        borderRadius: 2,
+                        boxShadow: '0 4px 10px rgba(0, 0, 0, 0.3)',
+                        margin: '16px',
+                        width: 'auto',
+                        maxWidth: 'none',
+                        height: 'calc(100% - 32px)',
+                    },
+                }}
             >
                 <IconButton
                     sx={{
                         position: 'absolute',
                         right: 8,
                         top: 8,
-                        color: 'white',
+                        color: 'theme.palette.grey[500]',
+                        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                        borderRadius: '50%',
+                        '&:hover': {
+                            backgroundColor: 'rgba(255, 255, 255, 0.9)',
+                        },
                     }}
                     onClick={handleClose}
                 >
                     <CloseIcon />
                 </IconButton>
-                <Box sx={{ p: 2, overflowY: 'auto' }}>
+                <Box
+                    sx={{
+                        p: 3,
+                        overflowY: 'auto',
+                        height: '100%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-between',
+                    }}
+                >
                     <CardMedia
                         component="img"
-                        sx={{ width: '100%', objectFit: 'cover' }}
+                        sx={{
+                            width: '100%',
+                            objectFit: 'cover',
+                            borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                        }}
                         image={croppedImg}
                         alt={name}
                     />
                     <Typography textAlign={"center"} variant="caption" display="block" sx={{ mt: 2 }}>
                         {furigana}
                     </Typography>
-                    <Typography textAlign={"center"} variant="h5" component="div" sx={{ mt: 1 }}>
+                    <Typography textAlign={"center"} variant="h5" component="div">
                         {name}
                     </Typography>
-                    <Typography textAlign={"center"} variant="h6" component="div" sx={{ mt: 2 }}>
+                    <Typography textAlign={"center"} variant="h6" component="div" >
                         {heading}
                     </Typography>
                     <Typography variant="body1" style={{ whiteSpace: 'pre-line' }} sx={{ mt: 2 }}>
